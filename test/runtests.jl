ad980490c96d0b1c659caa604857460d4a015d8f
Kevin R. Moore <kevmoor@sandia.gov>
2022-05-16 16:21:17 -0600

    Add inflow wind library and wrapper

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,18 @@
-using OpenFASTWrappers
+import OpenFASTWrappers
 using Test
+path,_ = splitdir(@__FILE__)
 
 @testset "OpenFASTWrappers.jl" begin
-    # Write your tests here.
-    # @test isapprox(a,b;atol=1e-4)
-    # include("./inflowwindtest.jl")
+    # Usage Example
+    # mkdir build
+    # cd build
+    # cmake ..
+    # make ifw_c_binding
+    # the the library is located in: deps/bin
+    turbsim_filename = "$path/data/ifw/test.bts"
+    OpenFASTWrappers.ifwinit(;turbsim_filename)
+    velocity = OpenFASTWrappers.ifwcalcoutput([0.0,0.0,100.0],0.1)
+    println(velocity)
+    OpenFASTWrappers.ifwend()
+    @test isapprox(velocity,[15.636677, -0.3423329, 0.24170564];atol=1e-4)
 end
