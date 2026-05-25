4fb5de610d286633b8990e0a08c769a669235dd7
kevmoor <64861600+kevmoor@users.noreply.github.com>
2026-05-25 14:09:18 -0600

    Skip native OpenFAST integration tests on Windows

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,19 +7,23 @@ include("root_motion_unit.jl")
 include("hawt_output_parity.jl")
 include("vawt_output_parity.jl")
 
-@testset "InflowWind" begin
-    include("ifw_run.jl")
-end
+if Sys.iswindows()
+    @info "Skipping native OpenFAST integration tests on Windows; helper and parser tests still run."
+else
+    @testset "InflowWind" begin
+        include("ifw_run.jl")
+    end
 
-@testset "HydroDyn" begin
-    include("hydrodyn_run.jl")
-end
+    @testset "HydroDyn" begin
+        include("hydrodyn_run.jl")
+    end
 
-@testset "MoorDyn" begin
-    include("moordyn_run.jl")
-end
+    @testset "MoorDyn" begin
+        include("moordyn_run.jl")
+    end
 
-@testset "AeroDyn" begin
-    include("aerodyn_run.jl")
-    include("aerodyn_run.jl") #Test running twice
+    @testset "AeroDyn" begin
+        include("aerodyn_run.jl")
+        include("aerodyn_run.jl") #Test running twice
+    end
 end
